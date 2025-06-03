cc6516526ebc2d18eac67663b68d07ef85516659
Jodus-Melodus <philipgricius@gmail.com>
2025-06-03 16:41:59 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use minifb::{Key, Window, WindowOptions};
 use rasterizer::Image;
 use std::io::Result;
 
-fn rasterize() -> Result<()> {
+fn main() -> Result<()> {
     let width = 512;
     let height = 512;
     let mut img = Image::new(width, height);
     let mut window = Window::new("Rasterizer", width, height, WindowOptions::default()).unwrap();
 
-    img.draw_triangle2((100, 100), (100, 300), (300, 100), Image::BLUE);
+    let points = [(100, 100), (400, 100), (100, 400), (400, 400)];
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         window
@@ -18,10 +18,5 @@ fn rasterize() -> Result<()> {
         std::thread::sleep(std::time::Duration::from_millis(30));
     }
 
-    Ok(())
-}
-
-fn main() -> Result<()> {
-    rasterize()?;
     Ok(())
 }
