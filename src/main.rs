af7a80a3ced8256ba31ad334de91463d8eae980b
Jodus-Melodus <philipgricius@gmail.com>
2025-06-03 19:15:53 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,19 @@
 mod rasterizer;
 use minifb::{Key, Window, WindowOptions};
 use rasterizer::Image;
-use std::{
-    io::Result,
-    time::{Duration, Instant},
-};
+use std::io::Result;
 
 fn main() -> Result<()> {
-    let width = 1024;
-    let height = 1024;
+    let width = 512;
+    let height = 512;
     let mut img = Image::new(width, height);
     let mut window = Window::new("Rasterizer", width, height, WindowOptions::default()).unwrap();
 
-    let points = [
-        (356, 256),
-        (327, 327),
-        (256, 356),
-        (185, 327),
-        (156, 256),
-        (185, 185),
-        (256, 156),
-        (327, 185),
-    ];
+    let points = [(500, 450), (200, 100), (100, 350)];
 
-    let start = Instant::now();
     for i in 1..points.len() - 1 {
-        img.draw_triangle1(points[0], points[i], points[i + 1], Image::RED);
+        img.draw_triangle1(points[0], points[i], points[i + 1], (50, 25, 150));
     }
-    let duration = start.elapsed().as_secs_f32();
-    let fps = 1.0 / duration;
-    println!("{:.2?}", fps);
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         window
