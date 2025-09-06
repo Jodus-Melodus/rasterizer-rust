d7d998dbf7abf9032c5d302c6327cca47cc46366
JodusMelodus <143637507+Jodus-Melodus@users.noreply.github.com>
2025-09-06 14:15:35 +0000

    remove container and basic window

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
-use crate::renderer::{screen::project_coordinate, vector::Vector3};
+use minifb::{Key, Window, WindowOptions};
+
+use crate::renderer::{screen::ScreenBuffer, types::Color, vector::Vector2};
 
 pub mod renderer;
 
-fn main() {
-    // let mut screen: ScreenBuffer<1280, 720> = ScreenBuffer::new();
-    // let a = Vector2::new(-10.0, -10.0);
-    // let b = Vector2::new(10.0, -10.0);
-    // let c = Vector2::new(0.0, 10.0);
-    // screen.draw_triangle(a, b, c, Color::WHITE);
-    // println!("{}", screen.ascii());
+const WIDTH: usize = 1280;
+const HEIGHT: usize = 720;
 
-    let point = Vector3::new(0.0, 0.0, -5.0);
-    println!("{:?}", point);
-    let projected_point = project_coordinate(point, 10.0);
-    println!("{:?}", projected_point);
+fn main() {
+    let mut window =
+        Window::new("rasterizer-rust", WIDTH, HEIGHT, WindowOptions::default()).unwrap();
+    let mut screen: ScreenBuffer<WIDTH, HEIGHT> = ScreenBuffer::new();
+    let a = Vector2::new(-10.0, -10.0);
+    let b = Vector2::new(10.0, -10.0);
+    let c = Vector2::new(0.0, 10.0);
+    screen.draw_triangle(a, b, c, Color::WHITE);
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        window
+            .update_with_buffer(&screen.pixels(), WIDTH, HEIGHT)
+            .unwrap();
+    }
 }
